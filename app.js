f423435f70c38a79a6496165c3f86703189db119
Modhura002 <modhura.banerjee@gmail.com>
2025-07-05 15:12:08 +0530

    logic modifications

--- app.js
+++ app.js
@@ -1,11 +1,34 @@
 let displayBtn=document.querySelector(".display");
 let numBtn=document.querySelectorAll(".buttons button");//buttons button to select the buttons. only buttons selects the container without the buttons
+let expression="";
 numBtn.forEach((btn)=>{
     btn.addEventListener("click",()=>{
-        let num=btn.getAttribute("id");
-         displayBtn.innerText+=num;
-        console.log(num);
+        let val=btn.getAttribute("id");
+         displayBtn.innerText+=val;
+        console.log(val);
+        if(val==="="){
+            calculate();}
+        else if(val==="C"){
+            displayBtn.innerText="";
+            expression="";
+        }
+        else{
+            expression+=val;
+        }
     });
 
 });
+
+const calculate=()=>{
+    try{
+   let result= eval(expression);
+   displayBtn.innerText=result;
+   console.log(result);}
+   catch(error){
+    displayBtn.innerText="error";
+    console.log("error");
+   }
+
+   
+}
 
